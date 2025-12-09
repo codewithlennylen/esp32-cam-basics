7494509c624eaf0d40a3fe91501fc550cb5d83f4
Lenny Ng'ang'a <codewithlennylen254@gmail.com>
2025-12-09 14:15:56 +0300

    Blink Sketch using Camera flash

---
+++ blink/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include "esp_camera.h"
+
+int cameraFlash = 4;
+
+void setup() {
+  pinMode(cameraFlash, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(cameraFlash, HIGH);
+  delay(1000);
+  digitalWrite(cameraFlash, LOW);
+  delay(1000);
+}
